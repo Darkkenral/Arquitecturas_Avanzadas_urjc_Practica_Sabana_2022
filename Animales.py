181740d156af381d5b994e6fcb5627ef4324e0e6
Miguel <miguelalbertolucea@hotmail.es>
2021-11-22 14:14:03 +0100

    Adelantada clase simulacion

--- Animales.py
+++ Animales.py
@@ -10,10 +10,33 @@ class Tipo(Enum):
     leon = 'Leon'
 
 
-class Animales(threading.Thread):
-    def __init__(self, sabana: simulacion, threadID, tipo: Tipo, posicion: tuple, manada):
+class Animal(threading.Thread):
+    '''
+    Clase animales, hereda de threads y es la clase padre de todos los animales del juego 
+
+    '''
+
+    def __init__(self, sabana: simulacion, threadID: int, tipo: Tipo, posicion: tuple, manada):
+        '''[summary]
+
+        Parameters
+        ----------
+        sabana : simulacion
+            [description]
+        threadID : int
+            [description]
+        tipo : Tipo
+            [description]
+        posicion : tuple
+            [description]
+        manada : [type]
+            [description]
+        '''
         self.sabana = sabana
         self.threadID = threadID
         self.tipo = tipo
         self.posicion = posicion
         self.manada = manada
+    
+    def __str__(self):
+        return str(self.tipo)+' (M:' +str(self.manada)+')'
--- Simulacion.py
+++ Simulacion.py
@@ -1,21 +1,75 @@
 import threading
 import time
-import Animales as animales
+from Animales import *
 
 
 class Casilla():
-    def __init__(self, animal, f, c):
+    def __init__(self, animal, c, f):
+        '''
+        Constructor de la clase casilla
+
+        Parameters
+        ----------
+        animal : [Animal]  
+            Animal que se ubica en la casilla 
+        c : [int]
+            [indice de columnas ]
+        f : [int]
+            [indice de filas]
+        '''
         self.animal = animal
         self.posicion = (f, c)
+        self.mutex = threading.Lock()
+
+    def get_animal():
+        return self.animal
+
+    def get_position():
+        return self.posicion
+
+    def get_mutex(args):
+        return self.mutex
+
+    def __str__(self):
+        return self.animal.__str__()
 
 
 class Mapa():
+    mapa = []
 
-    def __init__(self, f, c):
-        Self.mapa = [threading.Lock()] * f
-        for e in range(f):
-            e.append([threading.Lock()]*c)
-        return mapa
+    def __init__(self, c, f):
+        '''
+        Constructor de la clase mapa
+
+        Parametros
+        ----------
+        c : [int]
+            Numero de columnas de la matriz
+        f : [int]
+            Numero de filas de la matriz
+        '''
+        for i_c in range(c):
+            self.mapa.append(list())
+            for i_f in range(f):
+                self.mapa[i_c].append(Casilla(None, i_c, i_f))
+    
+
+    def get_Animal(self, posicion: tuple):
+        return self.mapa[posicion[0]][posicion[1]].get_animal()
+
+    def set_Animal(self, posicion: tuple, animal: Animal):
+        self.mapa[posicion[0]][posicion[1]] = animal
+
+    def delete_Animal(self, posicion: tuple):
+        self.mapa[posicion[0]][posicion[1]] = None
+
+    def popAnimal(self, posicion):
+        animal = self.get_Animal()
+        self.delete_Animal()
+        return animal
+
+    def __str__(self):
+        pass
 
 
 class Simulacion():
